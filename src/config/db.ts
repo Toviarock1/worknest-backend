e6069fa5685475537af8f4ebe95981d41e8e05bd
Toviarock1 <simontoviaadama@gmail.com>
2026-02-12 08:06:34 +0100

    fix: db

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,11 +4,18 @@ import { env } from "./env.js";
 import pg from "pg";
 
 const pool = new pg.Pool({
-  connectionString: env.DIRECT_URL,
+  connectionString: env.DATABASE_URL,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
   // ssl: {
   //   rejectUnauthorized: false,
   // },
 });
+
+// @ts-ignore - 'family' is a valid property in the actual pg driver, just missing in types
+pg.defaults.family = 4;
+
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
